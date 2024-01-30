07a653fbb75445402f8986c31fc7d47363ba2fcc
Savitha Raghunathan <saveetha13@gmail.com>
2024-01-30 09:08:25 -0500

    adding incident store

--- kai/util.py
+++ kai/util.py
@@ -1,12 +1,29 @@
 # This file contains utility functions that are used by the kai application
 
 import os
+
 import yaml
 
-from kai.application_hub import Application, ApplicationHub
-from kai.report import Report
+from kai.application_hub import ApplicationHub
+
 
 class Util:
+
+    def get_repo_path(app_name):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        # Get the parent directory of the current directory
+        parent_dir = os.path.dirname(dir_path)
+
+        # Construct the path to the sample repos directory and append the app_name
+        repo_path = os.path.join(parent_dir, "samples", "sample_repos", app_name)
+        return repo_path
+
+    def get_report_path(output_yaml_path):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        parent_dir = os.path.dirname(dir_path)
+        path = os.path.join(parent_dir, output_yaml_path)
+        return path
+
     def fetch_output_and_app_yaml(app_name):
         """
         Fetches the output.yaml file path and loads the contents of app.yaml
@@ -14,41 +31,44 @@ class Util:
         """
         # Path to the analysis_reports directory
         analysis_reports_dir = "samples/analysis_reports"
-        
+
         # Check if the specified app folder exists
         app_folder = os.path.join(analysis_reports_dir, app_name)
         if not os.path.exists(app_folder):
-            print(f"Error: {app_name} does not exist in the analysis_reports directory.")
+            print(
+                f"Error: {app_name} does not exist in the analysis_reports directory."
+            )
             return None
-        
+
         # Path to the output.yaml file
         output_yaml_path = os.path.join(app_folder, "output.yaml")
-        
+
         # Check if output.yaml exists for the specified app
         if not os.path.exists(output_yaml_path):
             print(f"Error: output.yaml does not exist for {app_name}.")
             return None
-        
+
         # Load contents of app.yaml
         app_yaml_path = os.path.join(app_folder, "app.yaml")
         if not os.path.exists(app_yaml_path):
             print(f"Error: app.yaml does not exist for {app_name}.")
             return None
-        
+
+        repo_path = Util.get_repo_path(app_name)
+
         with open(app_yaml_path, "r") as app_yaml_file:
             app_yaml_content = yaml.safe_load(app_yaml_file)
-        
+
         # Create a dictionary with file_path and repo keys
         output = {
-            "report_path": output_yaml_path,
-            "repo_path": app_yaml_content.get("repo", ""),
+            "report_path": Util.get_report_path(output_yaml_path),
+            "repo_path": repo_path,
             "initial_branch": app_yaml_content.get("initial_branch", ""),
             "solved_branch": app_yaml_content.get("solved_branch", ""),
             "commit_id": app_yaml_content.get("commit_id", ""),
-            "timestamp": app_yaml_content.get("timestamp", "")
-
+            "timestamp": app_yaml_content.get("timestamp", ""),
         }
-        
+
         return output
 
     def load_app_cached_violation(apps):
@@ -60,12 +80,15 @@ class Util:
         for app in apps:
             print(f"Loading application {app}\n")
             app_variables = Util.fetch_output_and_app_yaml(app)
-            report = Report(app_variables["report_path"])
-            formatted_report = report._read_report()
-            a = Application(app, formatted_report, app_variables["repo_path"], app_variables["initial_branch"], app_variables["solved_branch"], app_variables["commit_id"], app_variables["timestamp"])
-            application_hub.add_application(a)
+            application_hub.add_application(
+                app,
+                app_variables["report_path"],
+                app_variables["repo_path"],
+                app_variables["initial_branch"],
+                app_variables["solved_branch"],
+            )
+        print(f"Loaded incident store with {len(apps)} applications\n")
         return application_hub.cached_violations
-
 
     def write_cached_violations(cached_violations):
         """
@@ -73,19 +96,22 @@ class Util:
         """
 
         output_directory = "samples/generated_output/incident_store"
-        os.makedirs(output_directory, exist_ok=True)  # Create the directory if it doesn't exist
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        parent_dir = os.path.dirname(dir_path)
+        os.path.join(parent_dir, output_directory)
+        os.makedirs(
+            output_directory, exist_ok=True
+        )  # Create the directory if it doesn't exist
         output_file_path = os.path.join(output_directory, "cached_violations.yaml")
-        
+
         print(f"Writing incident store to file: {output_file_path}")
 
-        with open(output_file_path, 'w') as f:
+        with open(output_file_path, "w") as f:
             yaml.dump(cached_violations, f)
         print(f"Written cached_violations to {output_file_path}\n")
 
-    
     # todo: update cached_violations with the new application
-        
-    
+
     # determine if new application analysis report is available
     def is_new_analysis_report_available(application):
         # query hub api for the application
@@ -97,7 +123,7 @@ class Util:
             if app1.timestamp > application.timestamp:
                 return app1.timestamp
         return None
-    
+
     # update the timestamp in app.yaml if new analysis report is available
     def update_timestamp(application):
         # query hub api for the application
@@ -109,13 +135,15 @@ class Util:
             # update the timestamp in app.yaml
             # Path to the analysis_reports directory
             analysis_reports_dir = "samples/analysis_reports"
-            
+
             # Check if the specified app folder exists
             app_folder = os.path.join(analysis_reports_dir, application.name)
             if not os.path.exists(app_folder):
-                print(f"Error: {application.name} does not exist in the analysis_reports directory.")
+                print(
+                    f"Error: {application.name} does not exist in the analysis_reports directory."
+                )
                 return False
-                
+
             # Load contents of app.yaml
             app_yaml_path = os.path.join(app_folder, "app.yaml")
             if not os.path.exists(app_yaml_path):
@@ -124,16 +152,14 @@ class Util:
             # open the file in write mode and update the timestamp field
             with open(app_yaml_path, "r") as app_yaml_file:
                 app_yaml_data = yaml.safe_load(app_yaml_file)
-            
+
             # Update the timestamp in the app.yaml data
             app_yaml_data["timestamp"] = application.timestamp
-            
+
             # Write the updated contents back to app.yaml
             with open(app_yaml_path, "w") as app_yaml_file:
                 yaml.dump(app_yaml_data, app_yaml_file)
-            
+
             # Return true
             return True
         return False
-    
-    
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,38 +1,104 @@
-import unittest
-from kai.util import Util as util
-import kai.application_hub as ApplicationHub
-import kai.application_hub as Application
-
-import unittest
 import os
+import unittest
+
 import yaml
-import shutil
+
+from kai.util import Util as util
+
 
 class TestUtil(unittest.TestCase):
-    
-    
 
     def test_fetch_output_and_app_yaml_folder_not_exist(self):
         result = util.fetch_output_and_app_yaml("non_existent_app")
         self.assertIsNone(result)
-        
-        
+
+    def test_fetch_output_and_app_yaml_folder_exist(self):
+        result = util.fetch_output_and_app_yaml("kitchensink")
+        self.assertIsNotNone(result)
+
     def test_fetch_output_and_app_yaml_valid_input(self):
         result = util.fetch_output_and_app_yaml("kitchensink")
-        expected_output = {
-            "report_path": "samples/analysis_reports/kitchensink/output.yaml",
-            "repo_path": "https://github.com/tqvarnst/jboss-eap-quickstarts.git",
-            "initial_branch": "7.1",
-            "solved_branch": "quarkus-3.2",
-            "commit_id": None,
-            "timestamp": None
+        self.assertIsNotNone(result)
+
+    def test_fetch_output_and_app_yaml_invalid_input(self):
+        result = util.fetch_output_and_app_yaml("sample")
+        self.assertIsNone(result)
+
+    def test_load_app_cached_violation(self):
+        # Create a sample list of apps
+        apps = ["eap-coolstore-monolith"]
+        result = util.load_app_cached_violation(apps)
+        self.assertIsInstance(result, dict)
+        self.assertTrue(
+            result
+        )  # Check if result is not empty (non-None and non-empty dictionary)
+
+    def test_write_cached_violations(self):
+        test_cached_violations = {
+            "violation_1": {
+                "javaee-to-jakarta-namespaces-00001": {
+                    "eap-coolstore-monolith": {
+                        "webapp/WEB-INF/beans.xml": [
+                            {
+                                "variables": {
+                                    "matchingText": "http://xmlns.jcp.org/xml/ns/javaee"
+                                },
+                                "line_number": 18,
+                                "message": "Replace `http://xmlns.jcp.org/xml/ns/javaee` with `https://jakarta.ee/xml/ns/jakartaee` and change the schema version number ",
+                            },
+                            {
+                                "variables": {
+                                    "matchingText": "http://xmlns.jcp.org/xml/ns/javaee"
+                                },
+                                "line_number": 20,
+                                "message": "Replace `http://xmlns.jcp.org/xml/ns/javaee` with `https://jakarta.ee/xml/ns/jakartaee` and change the schema version number ",
+                            },
+                            {
+                                "variables": {
+                                    "matchingText": "http://xmlns.jcp.org/xml/ns/javaee"
+                                },
+                                "line_number": 21,
+                                "message": "Replace `http://xmlns.jcp.org/xml/ns/javaee` with `https://jakarta.ee/xml/ns/jakartaee` and change the schema version number ",
+                            },
+                        ]
+                    }
+                }
+            },
+            "violation_2": {
+                "javaee-to-jakarta-namespaces-00006": {
+                    "eap-coolstore-monolith": {
+                        "webapp/WEB-INF/beans.xml": [
+                            {
+                                "variables": {"matchingText": "beans_1_1.xsd"},
+                                "line_number": 21,
+                                "message": 'Replace `beans_1_1.xsd` with `beans_3_0.xsd` and update the version attribute to `"3.0"`',
+                            }
+                        ]
+                    }
+                }
+            },
         }
-        self.assertEqual(result, expected_output)
+        output_directory = "samples/generated_output/incident_store"
+        output_file_path = os.path.join(output_directory, "cached_violations.yaml")
 
-if __name__ == '__main__':
+        try:
+            # Call the function under test
+            util.write_cached_violations(test_cached_violations)
+
+            # Check if the file was created
+            print(output_file_path)
+            self.assertTrue(os.path.exists(output_file_path))
+
+            # Check if the written data matches the expected data
+            with open(output_file_path, "r") as f:
+                loaded_data = yaml.safe_load(f)
+            self.assertEqual(loaded_data, test_cached_violations)
+
+        finally:
+            # Clean up
+            if os.path.exists(output_file_path):
+                os.remove(output_file_path)
+
+
+if __name__ == "__main__":
     unittest.main()
-
-
-
-
-
